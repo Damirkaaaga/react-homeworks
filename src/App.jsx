b570b3299ea489370e91d57481fa819b94c37e3f
Дамир <suleimanovdamir21@gmail.com>
2025-03-22 17:18:58 +0200

    HW1: Changed heading text to trigger PR

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1>My List</h1>
+      <h1>My Fruit List</h1>
       <ul>
         {items.map((item, index) => (
           <li key={index}>{item}</li>
